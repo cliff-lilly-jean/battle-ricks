a385c7951c9207495679f8057fae649e146870d9
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-02-07 02:41:28 -0500

    Feature: added an method to create new computer players

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -3,16 +3,42 @@ import Player from '../Player/Player';
 
 function GameBoard() {
 
- // Todo: Prompt the user for a name on page load
- const COMPUTER = 'Dr. Robot_Nick';
+ // STATE
+ let [userPlayer, setUserPlayer] = useState('');
+ let [computerPlayer, setComputerPlayer] = useState();
 
+ // PROPERTIES
+ const computerPlayerNames = {
+  1: 'Dr.Robot_Nick',
+  2: '010110',
+  3: 'Process Sir',
+  4: 'Reboot'
+ };
+
+ // METHODS
+ let addNewComputerPlayer = () => {
+  let computerPlayerValues = Object.values(computerPlayerNames);
+  setComputerPlayer(computerPlayerValues[Math.floor(Math.random() * 3)]);
+ };
+
+ let addNewUserPlayer = () => {
+  let newPlayer = prompt();
+  setUserPlayer(newPlayer);
+ };
+
+
+ // useEffect Hook
+ useEffect(() => {
+  addNewUserPlayer();
+  addNewComputerPlayer();
+ }, []);
 
  return <div className="gameboard">
   {/* Return the two players of the game. */}
   {/* Computer Player */}
-  <Player playerType={COMPUTER} />
+  <Player playerType={computerPlayer} />
   {/* User Player */}
-  < Player playerType={user} />
+  < Player playerType={userPlayer} />
  </div>;
 }
 
