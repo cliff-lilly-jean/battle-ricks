8875e95140d33fb95fe92d73b216f0352e5e7671
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-02-28 19:16:56 -0500

    Merge branch 'main' of https://github.com/cliff-lilly-jean/battle-ricks into main

--- src/components/Deck/GetDeck.js
+++ src/components/Deck/GetDeck.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const GetDeck = () => {
 
+ const [apiUrls, setApiUrls] = useState([]);
  const [deck, setDeck] = useState([]);
 
  // The base url endpoint
@@ -12,16 +13,16 @@ const GetDeck = () => {
  // Calculate a random url to give to the getUrl function
  const calculateRandomUrl = (apiEndpoint) => {
   return `${apiEndpoint}${Math.floor(Math.random() * 42) + 1}`;
-
  };
 
- // Get a url to be uses to collect a random character
+ // Get a url to be used to collect a random character
  const getUrl = () => {
   return calculateRandomUrl(apiUrl);
  };
 
  const getRandomCharacterFromUrl = (resObj) => {
   const randomNumber = Math.floor(Math.random() * 20);
+
   const newCharacter = {
    name: resObj.data.results[randomNumber].name,
    status: resObj.data.results[randomNumber].status,
@@ -33,57 +34,27 @@ const GetDeck = () => {
   return newCharacter;
  };
 
- useEffect(() => {
+ const addUrlToArr = () => {
+  setApiUrls(apiUrls => [...apiUrls, getUrl()]);
+ };
+
+ useEffect(async () => {
 
  }, [deck]);
 
  // Get a new deck fo characters
  const getNewDeck = async () => {
 
-  // Loop through and add the data to the arrays
-  // for (let i = 0; i < 40; i++) {
-  //  setUrlEndpoints(urlEndpoints => [...urlEndpoints, `${apiUrl}${Math.floor(Math.random() * 42) + 1}`]);
-  //  setRandomCharacters(randomCharacters => [...randomCharacters, Math.floor(Math.random() * 20)]);
-  // }
+  for (let i = 0; i < 60; i++) {
+   addUrlToArr();
+  }
 
-  axios.get(getUrl()).then(res => {
-   for (let i = 0; i < 40; i++) {
-    getRandomCharacterFromUrl(res);
-    setDeck(deck => [...deck, getRandomCharacterFromUrl(res)]);
-   }
-
-   console.log('Deck :', deck);
-   // console.log()
+  Promise.all(apiUrls.map(url => axios.get(url).then(res => {
+   getRandomCharacterFromUrl(res);
+   setDeck(deck => [...deck, getRandomCharacterFromUrl(res)]);
   }).catch(error => {
    console.log(error);
-  });
-
-  // Use the all method on axios to get a new character page for all the endpoints in the urlEndpoints array
-  // axios.all(urlEndpoints).then(axios.spread((...allEndPoints) => {
-  //  allEndPoints.forEach((url, index) => {
-  //   axios.get(url).then((res) => {
-  //    let charactersIndex = randomCharacters[index];
-
-  //    if (typeof res.data.results[charactersIndex].name === undefined) {
-  //     res.data.results[charactersIndex].name = '?';
-  //    }
-
-  //    setDeck(deck => [...deck, {
-  //     name: res.data.results[charactersIndex].name,
-  //     status: res.data.results[charactersIndex].status,
-  //     species: res.data.results[charactersIndex].species,
-  //     type: res.data.results[charactersIndex].type,
-  //     gender: res.data.results[charactersIndex].gender,
-  //     image: res.data.results[charactersIndex].image,
-  //     location: res.data.results[charactersIndex].location.name,
-  //    }]);
-
-  //   }).catch(error => {
-  //    console.log(error);
-  //   });
-  //  });
-  // })
-  // );
+  })));
  };
 
  return { getNewDeck };
