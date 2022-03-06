4d70b9021386765f8f73c2cb223ddfd7f180ded7
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-03-06 08:07:13 -0500

    Feature: Deleted components folder data folder and function folder

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Button.css';
-
-const Button = ({ getNewDeck, updateCharactersRandomly }) => {
- return <button className='button' onClick={() => { getNewDeck(); }} > Start Game</button >;
-};
-
-export default Button;
