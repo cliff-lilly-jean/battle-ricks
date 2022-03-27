6be30c867bf56fb3fd1983ce0a597f586d8029e7
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-03-27 12:52:07 -0400

    updated the game components properties

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import { useState } from 'react/cjs/react.production.min';
+import { useEffect, useState } from 'react/cjs/react.production.min';
 import Board from '../Board/Board';
 
 const Game = () => {
 
  // The Game component contains the Board.
 
- // * Attributes
+ // * Properties
  // The Game has a start state
- const [gameStart, setGameStart] = useState(false);
+ const [startGame, setStartGame] = useState(false);
+
+ // The Game has a end state
+ const [endGame, setEndGame] = useState(true);
 
  // The Game has a win state
  const [winner, setWinner] = useState(null);
 
- // The Game can pick a winner
- let determineWinner = () => {
-  // If a Player's HP is 0, he lost
- };
-
- // The Game can start
- let start = () => {
-  // Change the gameStart state to true
-  // Create a new Board
-  // Create 2 new Player
-  // Create 2 new Deck
- };
-
  return (
-  <Board />
+  <div className="game-container">
+   <Board startGame={setStartGame} endGame={setEndGame} winner={setWinner} />
+  </div>
  );
 };
 
