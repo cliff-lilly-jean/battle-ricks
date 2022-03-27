57fc147a10d71e4520ae5e795416d39882152413
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-03-27 12:53:18 -0400

    added functions to start and end the game

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,32 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Player from '../Player/Player';
 
-const Board = () => {
+const Board = (startGame, endGame, setWinner) => {
 
  // The Board component contains an array, players of Player components.
 
- // * Attributes
+ // ! Properties
  // The Board has an array of Cell components
 
  // The Board has an array of Player
- const [players, setPlayers] = setState([]);
+ const [players, setPlayers] = useState([]);
 
 
- const createBoard = () => {
-  // Uses the Cell to populate the board squares
+ // ! Methods
+ const handleStartGame = () => {
+  startGame = true;
+  endGame = false;
+
+  // Run a function to  create the players
+  const createPlayers = () => {
+   // Creates a new User Player
+   // Creates a new AI Player
+   // Returns the players
+  };
+
+  // Run a function to create the board cell layout
+  const createBoard = () => {
+   // Uses the Cell to populate the board squares
+  };
+
+  console.log(startGame);
  };
 
- const createPlayers = () => {
-  // Creates a new User Player
-  // Creates a new AI Player
-  // Returns the players
+
+ const handleEndGame = () => {
+  startGame = false;
+  endGame = true;
+
+  console.log(endGame);
  };
 
+ const handleWinner = () => {
+
+ };
 
 
  return (
   <div className="board">
    {/* players array */}
+   <button onClick={handleStartGame}>
+    Start Game
+   </button>
   </div>
  );
 };
