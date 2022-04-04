d02786f0bea614e00b894667d57905e4c2b519c1
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-04-04 16:28:22 -0400

    Added the state for the player

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -6,12 +6,22 @@ const Player = () => {
 
  // The Player component has a Deck.
 
+ //  * Properties
+ // The Player component has a health point stat.
  const [healthPoints, setHealthPoints] = useState(8000);
 
- // The Player has a boolean isUser state
+ // The Player component has a Deck.
+ const [deck, setDeck] = useState('');
+
+ // The Player component can be a user of computer player.
  const [isUser, setIsUser] = useState(false);
 
- // * Attributes
+ // The Player component has a name.
+ const [playerName, setPlayerName] = useState('');
+
+
+
+ // * Methods
  // The Player can draw from the Deck
  const drawACard = () => {
   // Take a card from the top of the Deck
