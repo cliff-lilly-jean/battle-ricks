27d0ed01b47826717878ea937f77d950c9f0aeee
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-04-04 16:17:22 -0400

    Added a length and width to the Cell component

--- src/Components/Board/Cell/Cell.jsx
+++ src/Components/Board/Cell/Cell.jsx
@@ -4,8 +4,10 @@ const Cell = () => {
 
  // The Cell has a length and a width
 
- // * Attributes
+ // * Properties
  // A Cell has an array of 4 Sides.
+ const [length, setLength] = useState(0);
+ const [width, setWidth] = useState(0);
 
  return (
   <div>Cell</div>
