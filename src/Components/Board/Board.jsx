764e625ceb07ec13bd9a56ce1c57b113ad86d06e
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-04-05 22:04:02 -0400

    In process of refactoring

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Player from '../Player/Player';
 
-const Board = (startGame, endGame, winner) => {
+const Board = ({ endGame, winner, player1Deck, player1Hand, player2Deck, player2Hand, turn }) => {
 
  // * Properties
  // The Board has an array of Cell components
@@ -12,44 +12,49 @@ const Board = (startGame, endGame, winner) => {
 
 
  // * Methods
- const handleStartGame = () => {
-  startGame = true;
-  endGame = false;
+ // const handleStartGame = () => {
+ //  startGame = true;
+ //  endGame = false;
 
-  // Run a function to  create the players
-  const createPlayers = () => {
-   // Creates a new User Player
-   // Creates a new AI Player
-   // Returns the players
-  };
+ //  // Run a function to  create the players
+ //  const createPlayers = () => {
+ //   // Creates a new User Player
+ //   // Creates a new AI Player
+ //   // Returns the players
+ //  };
 
-  // Run a function to create the board cell layout
-  const createBoard = () => {
-   // Uses the Cell to populate the board squares
-  };
+ //  // Run a function to create the board cell layout
+ //  const createBoard = () => {
+ //   // Uses the Cell to populate the board squares
+ //  };
 
-  console.log(startGame);
- };
+ //  console.log(startGame);
+ // };
 
+ // const handleEndGame = () => {
+ //  startGame = false;
+ //  endGame = true;
 
- const handleEndGame = () => {
-  startGame = false;
-  endGame = true;
-
-  console.log(endGame);
- };
+ //  console.log(endGame);
+ // };
 
  const handleWinner = (playerGameWinner) => {
   winner = playerGameWinner;
  };
 
+ // const handlePlayerDeck = () => {
+ //  console.log(player1Deck);
+ //  console.log(player2Deck);
+ // };
+
+ useEffect(() => {
+
+ }, [turn, player1Deck, player2Deck]);
 
  return (
   <div className="board">
    {/* TODO: insert the players array */}
-   {/* <button onClick={handleStartGame}>
-    Start Game
-   </button> */}
+   {/* <button onClick={handlePlayerDeck}>Log player deck</button> */}
   </div>
  );
 };
