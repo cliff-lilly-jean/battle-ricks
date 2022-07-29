297b2d467003a1d67914db89512e6e35873be371
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2022-07-29 09:54:22 -0400

    Wrote out attack logic

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -25,7 +25,25 @@ const Card = ({ cardId, cardName, cardSpecies, cardAttackPoints, cardDefensePoin
       {/* Card Actions */}
       <button onClick={() => setChangeCardFaceDownPosition(!changeCardFaceDownPosition)} style={{ margin: "1rem auto", display: "block", background: "teal", padding: ".5rem 1.5rem", color: "white" }}>Change Face Position</button>
       <button onClick={() => setChangeCardAttackPosition(!changeCardAttackPosition)} style={{ margin: "1rem auto", display: "block", background: "olive", padding: ".5rem 1.5rem", color: "white" }}>Change Attack Position</button>
-      {/* TODO: Create a button and function for attack */}
+      {/* 
+      
+      TODO: Create attack functionality
+
+        - allow the user to select a card on the opponent field
+        - allow the user to chose a card (in attack mode) on their field
+        - subtract the values
+          - if the opponent card wins, destroy the attackers card
+            - add the card to the attackers graveyard
+          - if the opponent loses,
+            - if the card is in defense position  and face down, the card is destroyed
+            - if the card is defense position and face up, subtract the attackers attack value against the opponent defense value
+              - if the defense value is larger, the attackers card is destroyed and LP reflect the difference
+              - if the defense value is smaller, the defense card is destroyed and the opponent LP reflects the difference
+            - if the card is in attack position (face up or face down),
+              - the attack values are subtracted and the lower attack value card is destroyed and the LP of the loser is reflected
+      
+      
+      */}
     </div >
   );
 };
