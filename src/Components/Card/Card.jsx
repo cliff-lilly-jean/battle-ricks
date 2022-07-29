9b7b8cb159c75df379ed50beded91bb9bb786565
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2022-07-29 05:57:24 -0400

    Beginning to build out the card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import cardStrengthWeakness from '../../Data/Card/cardSpeciesStrengthAndWeakness';
 import cardSpeciesColors from '../../Data/Card/cardSpeciesColors';
-import cardGenderIcons from '../../Data/Card/cardGenderIcons';
-import buildACard from '../../Functions/Card/buildACard';
 
 
 
-const Card = ({ generateCards }) => {
 
-  const [faceDown, setFaceDown] = useState(true);
-  const [attackPosition, setAttackPosition] = useState(false);
-  // Hope this fixes it
+const Card = ({ cardId, cardName, cardSpecies, cardAttackPoints, cardDefensePoints, cardHealthStatus, cardGenderIcon, cardImage, cardHomeLocation, cardFaceDownPosition, cardAttackPosition }) => {
 
-  let card = buildACard(generateCards, cardSpeciesColors, cardGenderIcons, faceDown, attackPosition, setFaceDown, setAttackPosition);
+  // const [faceDown, setFaceDown] = useState(true);
+  // const [attackPosition, setAttackPosition] = useState(false);
 
 
   return (
-    <div className='findCard-card ml-8' >
-      {card}
+    <div className='findCard-card ml-8' style={{ background: cardSpeciesColors[cardSpecies], padding: "1rem 1.5rem", maxWidth: "10000px", margin: "0 auto" }}>
+      <div className="cardContent" style={{}}>
+        <p>{`Card id: ${cardId}`}</p>
+        <p>{`Card name: ${cardName}`}</p>
+        <p>{`Card species: ${cardSpecies}`}</p>
+        <p>{`Card attack: ${cardAttackPoints}`}</p>
+        <p>{`Card defense: ${cardDefensePoints}`}</p>
+        <p>{`Card status: ${cardHealthStatus}`}</p>
+        <p>{`Card gender icon:`} {cardGenderIcon}</p>
+        <p>{`Card image:`} {cardImage}</p>
+        <p>{`Card location: ${cardHomeLocation}`}</p>
+        <p>{`Card face down: ${cardFaceDownPosition}`}</p>
+        <p>{`Card attack position:`} {cardAttackPosition}</p>
+      </div>
+      <button onClick={() => console.log(`attack: ${cardAttackPoints}\nId: ${cardId}`)} style={{ margin: "1rem auto", display: "block", background: "teal", padding: ".5rem 1.5rem", color: "white" }}>Attack</button>
     </div >
   );
 };
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -4,18 +4,16 @@ import Card from '../Card/Card';
 import cardStrengthWeakness from '../../Data/Card/cardSpeciesStrengthAndWeakness';
 import cardSpeciesColors from '../../Data/Card/cardSpeciesColors';
 import cardGenderIcons from '../../Data/Card/cardGenderIcons';
-import buildACard from '../../Functions/Card/buildACard';
+
 
 const Cards = ({ generateCards }) => {
 
- const [faceDown, setFaceDown] = useState(true);
- const [attackPosition, setAttackPosition] = useState(false);
 
- return generateCards.map((card) => {
-  return (
-   <Card key={card.id} style={{ background: cardSpeciesColors[card.species] }} cardId={card.id} cardName={card.name} cardSpecies={card.species} cardAttackPoints={card.attack} cardDefensePoints={card.defense} cardHealthStatus={card.status} cardGenderIcon={<img src={cardGenderIcons[card.gender]} alt="Gender icon" width="25" />} cardImage={<img src={card.image} alt="" />} cardHomeLocation={card.location} cardFaceDownPosition={card.faceDown} cardAttackPosition={card.attackPosition} />
-  );
- });
+    return generateCards.map((card) => {
+        return (
+            <Card key={card.id} cardId={card.id} cardName={card.name} cardSpecies={card.species} cardAttackPoints={card.attack} cardDefensePoints={card.defense} cardHealthStatus={card.status} cardGenderIcon={<img src={cardGenderIcons[card.gender]} alt="Gender icon" width="25" />} cardImage={<img src={card.image} alt="" />} cardHomeLocation={card.location} cardFaceDownPosition={card.faceDown} cardAttackPosition={card.attackPosition} />
+        );
+    });
 
 
 };
