3799881de08aac37cd08628910b4a850bba1b9be
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-07-31 11:16:12 -0400

    updated the cardSpeciesStrengthsAndWeakneses method

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import cardSpeciesColors from '../../Data/Card/cardSpeciesColors';
+import cardSpeciesStrengthWeakness from '../../Data/Card/cardSpeciesStrengthAndWeakness';
 
 
-const Card = ({ cardId, cardName, cardSpecies, cardAttackPoints, cardDefensePoints, cardHealthStatus, cardGenderIcon, cardImage, cardHomeLocation, cardFaceDownPosition, cardAttackPosition }) => {
+const Card = ({ cardName, cardSpecies, cardAttackPoints, cardDefensePoints, cardHealthStatus, cardGenderIcon, cardImage, cardHomeLocation, cardFaceDownPosition, cardAttackPosition }) => {
 
-  const [changeCardFaceDownPosition, setChangeCardFaceDownPosition] = useState(cardFaceDownPosition);
-  const [changeCardAttackPosition, setChangeCardAttackPosition] = useState(cardAttackPosition);
+ const [changeCardFaceDownPosition, setChangeCardFaceDownPosition] = useState(cardFaceDownPosition);
+ const [changeCardAttackPosition, setChangeCardAttackPosition] = useState(cardAttackPosition);
 
-  return (
-    <div className='findCard-card ml-8' style={{ background: cardSpeciesColors[cardSpecies], padding: "1rem 1.5rem", maxWidth: "10000px", margin: "0 auto" }}>
-      <div className="cardContent" style={{}}>
-        <p>{`Name: ${cardName}`}</p>
-        <p>{`Image:`} {cardImage}</p>
-        <p>{`Species: ${cardSpecies}`}</p>
-        <p>{`Attack: ${cardAttackPoints}`}</p>
-        <p>{`Defense: ${cardDefensePoints}`}</p>
-        <p>{`Status: ${cardHealthStatus}`}</p>
-        <p>{`Gender:`} {cardGenderIcon}</p>
-        <p>{`Home: ${cardHomeLocation}`}</p>
-        <p>{`Face: ${changeCardFaceDownPosition ? "Face Down" : "Face Up"}`} </p>
-        <p>{`Position: ${changeCardAttackPosition ? "Attack Position" : "Defense Position"}`}</p>
-      </div>
+ return (
+  <div className='findCard-card ml-8' style={{ background: cardSpeciesColors[cardSpecies], padding: "1rem 1.5rem", maxWidth: "10000px", margin: "0 auto" }}>
+   <div className="cardContent" style={{}}>
+    <p>{`Name: ${cardName}`}</p>
+    <p>{`Image:`} {cardImage}</p>
+    <p>{`Species: ${cardSpecies}`}</p>
+    <p>{`Attack: ${cardAttackPoints}`}</p>
+    <p>{`Defense: ${cardDefensePoints}`}</p>
+    <p>{`Status: ${cardHealthStatus}`}</p>
+    <p>{`Gender:`} {cardGenderIcon}</p>
+    <p>{`Home: ${cardHomeLocation}`}</p>
+    <p>{`Face: ${changeCardFaceDownPosition ? "Face Down" : "Face Up"}`} </p>
+    <p>{`Position: ${changeCardAttackPosition ? "Attack Position" : "Defense Position"}`}</p>
+    { }
+   </div>
 
-      {/* Card Actions */}
-      <button onClick={() => setChangeCardFaceDownPosition(!changeCardFaceDownPosition)} style={{ margin: "1rem auto", display: "block", background: "teal", padding: ".5rem 1.5rem", color: "white" }}>Change Face Position</button>
-      <button onClick={() => setChangeCardAttackPosition(!changeCardAttackPosition)} style={{ margin: "1rem auto", display: "block", background: "olive", padding: ".5rem 1.5rem", color: "white" }}>Change Attack Position</button>
-      {/* 
-      
+   {/* Card Actions */}
+   <button onClick={() => setChangeCardFaceDownPosition(!changeCardFaceDownPosition)} style={{ margin: "1rem auto", display: "block", background: "teal", padding: ".5rem 1.5rem", color: "white" }}>Change Face Position</button>
+   <button onClick={() => setChangeCardAttackPosition(!changeCardAttackPosition)} style={{ margin: "1rem auto", display: "block", background: "olive", padding: ".5rem 1.5rem", color: "white" }}>Change Attack Position</button>
+   <button onClick={() => console.log(`Card attack: ${cardAttackPoints} \nCard attack  with bonus: ${cardSpeciesStrengthWeakness.Human.strongAgainst.Animal() + cardAttackPoints}`)} style={{ margin: "1rem auto", display: "block", background: "olive", padding: ".5rem 1.5rem", color: "white" }}>Attack</button>
+   {/*
+
       TODO: Create attack functionality
 
         - allow the user to select a card on the opponent field
@@ -41,11 +44,11 @@ const Card = ({ cardId, cardName, cardSpecies, cardAttackPoints, cardDefensePoin
               - if the defense value is smaller, the defense card is destroyed and the opponent LP reflects the difference
             - if the card is in attack position (face up or face down),
               - the attack values are subtracted and the lower attack value card is destroyed and the LP of the loser is reflected
-      
-      
+
+
       */}
-    </div >
-  );
+  </div >
+ );
 };
 
 export default Card;
