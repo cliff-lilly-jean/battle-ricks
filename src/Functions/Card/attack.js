0388389f0ef0b1091f6b1a4d11a67670e7144538
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2022-08-02 20:14:56 -0400

    attack method updates

--- src/Functions/Card/attack.js
+++ src/Functions/Card/attack.js
@@ -1,25 +1,43 @@
 
 
-const attack = (attackCardSpecies, attackCardAttackPoints, enemyCardSpecies, enemyCardFace, enemyCardPosition, cardStrengthsObj) => {
+const attack = (attackCardSpecies, attackCardAttackPoints, enemyCardSpecies, enemyCardFace, enemyCardPosition, enemyCardDefensePoints, cardStrengthsObj) => {
 
-    // TODO:
-    // Determine Enemy card face
+    // Get the player to select an enemyCard
+    // 
+    // const strongerThanArr = cardStrengthsObj[attackCardSpecies][0].strongAgainst;
+    // // Determine Enemy card face
+
+    // // If enemyCardFace is true, the card is face down
+    // if (enemyCardFace === true) {
+    //     // TODO: call a flip card animation function
+
+    //     // If enemyCardPosition is false, the card is in defense position
+    //     if (enemyCardPosition === false) {
+
+
+    //         // Determine if attack card species is stronger than enemy card species
+    //         let res = strongerThanArr.find(enemy => enemy === enemyCardSpecies);
+    //         let totalAttack = attackCardAttackPoints - enemyCardDefensePoints;
+
+    //         if (strongerThanArr.length <= 0) {
+    //             return totalAttack;
+    //         } else if (res === undefined) {
+    //             return totalAttack;
+    //         } else {
+    //             let attackBonus = Math.floor(Math.random() * 200);
+    //             let totalAttackWithBonus = (attackCardAttackPoints + attackBonus) - enemyCardDefensePoints;
+    //             return totalAttackWithBonus;
+    //         }
+    //     }
+    // }
+
+
+
     // Determine enemy card position
     // Make logic for facedown attack and defense, faceup attack and defense
 
 
-    // Determine if attack card species is stronger than enemy card species
-    const strongerThanArr = cardStrengthsObj[attackCardSpecies][0].strongAgainst;
-    let res = strongerThanArr.find(enemy => enemy === enemyCardSpecies);
-    if (strongerThanArr.length <= 0) {
-        return attackCardAttackPoints;
-    } else if (res === undefined) {
-        return attackCardAttackPoints;
-    } else {
-        let attackBonus = Math.floor(Math.random() * 200);
-        let totalAttackWithBonus = attackCardAttackPoints + attackBonus;
-        return totalAttackWithBonus;
-    }
+
 
 };
 
